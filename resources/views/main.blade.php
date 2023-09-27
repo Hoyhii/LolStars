82ec69b957b8c6a7f619df27675526db1f6628a7
Hoyhii <Hoyhitv@gmail.com>
2023-09-27 08:39:28 +0200

    Main page, User profile modification.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,29 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Stars</h1>
+    <div class="container">
+        <h1>Stars</h1>
 
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Country</th>
-            <th>Nickname</th>
-            <th>Region</th>
-            <th>Role</th>
-            <th>Team</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($users as $user)
+        <table class="table table-striped">
+            <thead>
             <tr>
-                <td>{{ $user->country }}</td>
-                <td><a href="{{ route('players.show', $user->id) }}" class="btn btn-info">{{ $user->name }}</a></td>
-                <td>{{ $user->region }}</td> <!-- Most akkor, legyen main acc region, vagy a főoldalon ne jelenjen meg? ha igen akkor a fő acc regiont külön tárolni és kiiratni kéne -->
-                <td>{{ $user->role }}</td>
-                <td>{{ $user->team }}</td>
-                <!-- Egyéb mezők... -->
+                <th>Country</th>
+                <th>Nickname</th>
+                <th>Region</th>
+                <th>Role</th>
+                <th>Team</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->country }}</td>
+                    <td><a href="{{ route('players.show', $user->id) }}" class="btn btn-info">{{ $user->name }}</a></td>
+                    <td>{{ $user->region }}</td>
+                    <td>{{ $user->role }}</td>
+                    <td>{{ $user->team }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Country</th>
-            <th>Nickname</th>
-            <th>Region</th>
-            <th>Role</th>
-            <th>Team</th>
-            <th>Youtube</th>
-            <th>Twitch</th>
-            <th>Discord</th>
-            <th>Twitter</th>
-            <th>Leaguepedia</th>
-            <th>Summoners</th>
-            <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
+    <div class="container">
+        <h1>Player Profile</h1>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>Country</th>
+                <th>Nickname</th>
+                <th>Region</th>
+                <th>Role</th>
+                <th>Team</th>
+                <th>Youtube</th>
+                <th>Twitch</th>
+                <th>Discord</th>
+                <th>Twitter</th>
+                <th>Leaguepedia</th>
+                <th>Summoners</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
             <tr>
                 <td>{{ $user->country }}</td>
                 <td><a href="{{ route('players.show', $user->id) }}" class="btn btn-info">{{ $user->name }}</a></td>
@@ -33,12 +35,13 @@
                 <td>
                     @foreach($summoners as $summoner)
                         {{ $summoner->summoner_name }}, {{ $summoner->region }}
-                        <a href="{{ route('summoner.edit', $summoner->id) }}" class="btn btn-primary">Edit Summoner</a>|||
+                        <a href="{{ route('summoner.edit', $summoner->id) }}" class="btn btn-primary btn-sm ml-2">Edit</a>
+                        <br>
                     @endforeach
                 </td>
                 <td>
                     <a href="{{ route('summoner.create') }}" class="btn btn-primary">Add Summoner</a>
-                    <a href="{{ route('players.edit', $user->id) }}" class="btn btn-primary">Edit</a>
+                    <a href="{{ route('players.edit', $user->id) }}" class="btn btn-primary">Edit Profile</a>
                     <form action="{{ route('players.destroy', $user->id) }}" method="POST" style="display: inline">
                         @csrf
                         @method('DELETE')
@@ -46,6 +49,7 @@
                     </form>
                 </td>
             </tr>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 @endsection
